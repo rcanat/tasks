3073d9ac65f4131d7bb9737c233c51d865c75fd7
Ryan Canatella <rcanat@udel.edu>
2026-02-24 15:05:58 -0500

    Completed sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -63,7 +63,7 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return sumPoints(getPublishedQuestions(questions));
 }
 
 /***
