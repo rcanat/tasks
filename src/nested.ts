c2fb7227d607837afe8f9470293596ee9871ae6b
Ryan Canatella <rcanat@udel.edu>
2026-02-24 16:02:58 -0500

    Completed duplicateQuestionInArray

--- src/nested.ts
+++ src/nested.ts
@@ -1,6 +1,6 @@
 import { Answer } from "./interfaces/answer";
 import { Question, QuestionType } from "./interfaces/question";
-import { makeBlankQuestion } from "./objects";
+import { duplicateQuestion, makeBlankQuestion } from "./objects";
 
 /**
  * Consumes an array of questions and returns a new array with only the questions
@@ -244,5 +244,11 @@ export function duplicateQuestionInArray(
     targetId: number,
     newId: number,
 ): Question[] {
-    return [];
+    return questions.reduce(
+        (arr: Question[], q: Question) =>
+            q.id === targetId ?
+                (arr = [...arr, q, duplicateQuestion(newId, q)])
+            :   [...arr, q],
+        [],
+    );
 }
