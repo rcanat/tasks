9c2c56b42f65a2e8831c482c079f552be1f14aa8
Ryan Canatella <rcanat@udel.edu>
2026-03-09 00:05:07 -0400

    Completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,42 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🎄" | "🥚" | "🎃" | "🪩" | "🦃";
+    const [holiday, setHoliday] = useState<Holiday>("🪩");
+
+    const nextYear: Record<Holiday, Holiday> = {
+        "🪩": "🥚",
+        "🥚": "🎃",
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🪩",
+    };
+
+    const nextAlphabet: Record<Holiday, Holiday> = {
+        "🎄": "🥚",
+        "🥚": "🎃",
+        "🎃": "🪩",
+        "🪩": "🦃",
+        "🦃": "🎄",
+    };
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setHoliday(nextAlphabet[holiday]);
+                }}
+            >
+                Advance by Alphabet
+            </Button>
+            <Button
+                onClick={() => {
+                    setHoliday(nextYear[holiday]);
+                }}
+            >
+                Advance by Year
+            </Button>
+            <p>Holiday: {holiday}</p>
+        </div>
+    );
 }
